2811c3eeea2a960602b266da8fb3af072a3a2f3d
Khroxx <134767175+Khroxx@users.noreply.github.com>
2024-11-08 13:03:30 +0100

    started notely backend using docker
    pre-commit, docker running locally

---
+++ notely/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from notely.users.models import User
+from notely.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
